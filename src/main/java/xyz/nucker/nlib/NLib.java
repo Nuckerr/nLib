302109a5d2b0625b5cbd00202093d8507d232e1f
Daniel <nuckermail@gmail.com>
2020-06-24 05:45:29 +0100

    Removed plugin.yml. Changed version

--- src/main/java/xyz/nucker/nlib/NLib.java
+++ src/main/java/xyz/nucker/nlib/NLib.java
@@ -2,6 +2,8 @@ package xyz.nucker.nlib;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -11,7 +13,11 @@ public final class NLib extends JavaPlugin {
     public String msg(String message) {
         return ChatColor.translateAlternateColorCodes('&', message);
     }
+
     public void log(String message) {
-        Bukkit.getLogger().info(ChatColor.translateAlternateColorCodes('&',message));
+        Bukkit.getLogger().info(ChatColor.translateAlternateColorCodes('&', message));
+    }
+
+    public void reloadCommand() {
     }
 }
